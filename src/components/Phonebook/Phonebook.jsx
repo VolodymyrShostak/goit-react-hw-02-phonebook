b6dd60e550506cf706aa7c2baf16d537707d3b43
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-11-21 14:30:02 +0200

    add

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,54 +1,70 @@
 import React from 'react';
-// import { nanoid } from 'nanoid';
+import { nanoid } from 'nanoid';
 
 class Phonebook extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      contacts: [],
-      name: '',
-    };
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleAddContact = this.handleAddContact.bind(this);
-  }
+  state = {
+    contacts: [],
+    name: '',
+    number: '',
+  };
 
-  handleInputChange(event) {
+  onNameChange = event => {
     this.setState({
       name: event.target.value,
+      id: nanoid(),
     });
-  }
-  handleAddContact(event) {
-    const { name, contacts } = this.state;
+  };
+  handleNumberChange = event => {
+    this.setState({
+      number: event.target.value,
+      id: nanoid(),
+    });
+  };
+  onAddContact = event => {
+    const { name, contacts, number } = this.state;
     event.preventDefault();
     this.setState({
-      name: "",
-      contacts: [...contacts, name],
+      contacts: [...contacts, name, number],
+      name: '',
+      number: '',
     });
-  }
+  };
   render() {
-    const { name, contacts } = this.state;
+    const { name, contacts, id, number } = this.state;
     return (
       <div>
         <h2>Phonebook</h2>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.onAddContact}>
           <h3>Name</h3>
           <input
             type="text"
             name="name"
             value={name}
-            onChange={this.handleInputChange}
+            onChange={this.onNameChange}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
           />
-          <button type="submit" onClick={this.handleAddContact}>
+          <h3>Number</h3>
+          <input
+            type="tel"
+            name="number"
+            value={number}
+            onChange={this.handleNumberChange}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+          <button type="submit" onClick={this.onAddContact}>
             Add contact
           </button>
         </form>
         <h2>Contacts</h2>
         <ul>
           {contacts.map((contact, index) => (
-            <li key={index}>{contact}</li>
+            <li key={index} id={id}>
+              {contact} {number}
+            </li>
           ))}
         </ul>
       </div>
